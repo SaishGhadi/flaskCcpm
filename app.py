7fe5ac348883c9f627c9d2da46d624d20c98c315
Ghadi <ghadi@example.com>
2025-06-16 11:56:24 +0530

    Initial commit - Flask app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for Laravel access
+
+model = joblib.load('cc_price_model.pkl')
+
+@app.route('/')
+def home():
+    return "Carbon Credit Price Prediction API is running."
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+
+        input_df = pd.DataFrame([data])
+
+        prediction = model.predict(input_df)
+        return jsonify({'predicted_price': prediction[0]})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
